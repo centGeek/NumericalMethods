a1d1bdd589aae42267110d0ac3bcc87c3285e904
WojciechBlyskal <161322394+WojciechBlyskal@users.noreply.github.com>
2024-03-01 20:32:37 +0100

    Implementacja stycznych

--- exercise 1/main.py
+++ exercise 1/main.py
@@ -16,18 +16,25 @@ def bisekcja(funkcja, a, b):
         a.append(srodek)
     return srodek
 
+def styczne(funkcja, funkcja_pochodna, xi):
+    return xi - funkcja(xi) / funkcja_pochodna(xi)
+
 def sinus(x):
     return math.cos(x)
+
+def poch_sinus(x):
+    return (math.sin(x) * (-1))
 
 wybrana_funkcja_string = input("Podaj nr funkcji ktora chcesz wybrac: '1'.")
 if wybrana_funkcja_string == '1':
     wybrana_funkcja = sinus;
+    wybrana_funkcja_poch = poch_sinus;
 
-lewy_koniec_przedzialu_string = input("Podaj najmniejsza wartosc x, ktora bierzemy pod uwage(lewy kraniec przedzialu:)")
+lewy_koniec_przedzialu_string = input("Podaj najmniejsza wartosc x, ktora bierzemy pod uwage(lewy kraniec przedzialu):")
 lewy_koniec_przedzialu = []
 lewy_koniec_przedzialu.append(konwersja_string_na_double(lewy_koniec_przedzialu_string))
 
-prawy_koniec_przedzialu_string = input("Podaj najwieksza wartosc x, ktora bierzemy pod uwage(prawy kraniec przedzialu:)")
+prawy_koniec_przedzialu_string = input("Podaj najwieksza wartosc x, ktora bierzemy pod uwage(prawy kraniec przedzialu):")
 prawy_koniec_przedzialu = []
 prawy_koniec_przedzialu.append(konwersja_string_na_double(prawy_koniec_przedzialu_string))
 
@@ -41,14 +48,39 @@ else:
     if epsilon_czy_ilosc_iteracji == '1':
         epsilon_string = input("Podaj dokladnosc, z jaka ma byc wykonany algorytm:")
         epsilon = konwersja_string_na_double(epsilon_string)
-        while abs(wybrana_funkcja(miejsce_zerowe)) >= epsilon:
-            miejsce_zerowe = bisekcja(wybrana_funkcja, lewy_koniec_przedzialu, prawy_koniec_przedzialu) #indeks -1 w python oznacza ostatni element tablicy
-        print("Miejscem zerowym funkcji jest:" + str(miejsce_zerowe))
+        wybor_metody = input ("Jesli chcesz, by zastosowano metode bisekcji, napisz '1', jesli chcesz, by algorytm zastosowal metode stycznych, napisz cokolwiek innego:")
+        if wybor_metody == '1':
+            while abs(wybrana_funkcja(miejsce_zerowe)) >= epsilon:
+                miejsce_zerowe = bisekcja(wybrana_funkcja, lewy_koniec_przedzialu, prawy_koniec_przedzialu) #indeks -1 w python oznacza ostatni element tablicy
+            print("Miejscem zerowym funkcji jest:" + str(miejsce_zerowe))
+        else:
+            x_poczatkowe = input("Podaj poczatkowa wartosc x:")
+            miejsce_zerowe_s = [konwersja_string_na_double(x_poczatkowe)]
+            while abs(wybrana_funkcja(miejsce_zerowe_s[-1])) >= epsilon:
+                if wybrana_funkcja_poch(miejsce_zerowe_s[-1] == 0):
+                    print("Przerwano wykonywanie algorytmu, poniewaz pochodna funkcji wynosi 0.")
+                    epsilon = float('inf')
+                else:
+                    miejsce_zerowe_s.append(styczne(wybrana_funkcja, wybrana_funkcja_poch, miejsce_zerowe_s[-1]))
+                    print("Miejscem zerowym funkcji jest:" + str(miejsce_zerowe_s[-1]))
     else:
         ilosc_iteracji_string = input("Podaj ilosc iteracji, po ktorych ma sie zakonczyc algorytm:")
         ilosc_iteracji = konwersja_string_na_integer(ilosc_iteracji_string)
         iterator = 0
-        while ilosc_iteracji > iterator:
-            miejsce_zerowe = bisekcja(wybrana_funkcja, lewy_koniec_przedzialu, prawy_koniec_przedzialu)
-            iterator = iterator + 1
-        print("Miejscem zerowym funkcji jest:" + str(miejsce_zerowe))
+        wybor_metody = input ("Jesli chcesz, by zastosowano metode bisekcji, napisz '1', jesli chcesz, by algorytm zastosowal metode stycznych, napisz cokolwiek innego:")
+        if wybor_metody == '1':
+            while ilosc_iteracji > iterator:
+                miejsce_zerowe = bisekcja(wybrana_funkcja, lewy_koniec_przedzialu, prawy_koniec_przedzialu) #indeks -1 w python oznacza ostatni element tablicy
+                iterator += 1
+            print("Miejscem zerowym funkcji jest:" + str(miejsce_zerowe))
+        else:
+            x_poczatkowe = input("Podaj poczatkowa wartosc x:")
+            miejsce_zerowe_s = [konwersja_string_na_double(x_poczatkowe)]
+            while ilosc_iteracji > iterator:
+                if wybrana_funkcja_poch(miejsce_zerowe_s[-1] == 0):
+                    print("Przerwano wykonywanie algorytmu, poniewaz pochodna funkcji wynosi 0.")
+                    iterator = ilosc_iteracji
+                else:
+                    miejsce_zerowe_s.append(styczne(wybrana_funkcja, wybrana_funkcja_poch, miejsce_zerowe_s[-1]))
+                    iterator += 1
+                    print("Miejscem zerowym funkcji jest:" + str(miejsce_zerowe_s[-1]))
