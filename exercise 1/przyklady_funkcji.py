c826c13fae86aba2e0714a0506c112b44be341ec
WojciechBlyskal <161322394+WojciechBlyskal@users.noreply.github.com>
2024-03-15 15:04:52 +0100

    Update przyklady_funkcji.py

--- exercise 1/przyklady_funkcji.py
+++ exercise 1/przyklady_funkcji.py
@@ -6,13 +6,16 @@ def sinus(x):
 
 def wielomian(x):
     return x * x * (x - 2) - 5
+
 def poch_wielomian(x):
-    return 3 * x * x - 4 * x
+    return x * (3 * x - 4)
 
 def wykladnicza(x):
     return np.exp(x) - 2
+
 def pochodna_wykladniczej(x):
-    return np.e ** x
+    return np.exp(x)
 
 def poch_sinus(x):
     return 5 * np.cos(x)
+    
