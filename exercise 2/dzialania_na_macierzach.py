45e3b5acdc9c1e616cdf94534636cf44cad26c96
WojciechBlyskal <wojtekblyskal03@gmail.com>
2024-04-03 22:21:57 +0200

    skonczone

--- exercise 2/dzialania_na_macierzach.py
+++ exercise 2/dzialania_na_macierzach.py
@@ -6,29 +6,54 @@ class Dzialania_na_macierzach:
     def __init__(self):
         init()
 
-    """def switch_rows(matrix, row1, row2):
+    def switch_rows(self, matrix, row1, row2): #odpowiada za zamiane wierszy w macierzy
         matrix2 = [row[:] for row in matrix]
         for j in range(len(matrix[0])):
-            matrix[row1][j], matrix[row2][j] = matrix2[row2][j], matrix2[row1][j]"""
+            matrix[row1][j], matrix[row2][j] = matrix2[row2][j], matrix2[row1][j]
 
-    def subtraction(matrix, base_row_index, subtracted_row_index, amount): #base_row to wiersz z odjemnnymi, subtracted_row z odjemnikami
-        base_row = []
-        subtracted_row = []
-        for row_index, row in enumerate(matrix): #wydobywamy wlasciwe wiersze
-            if row_index == subtracted_row_index:
-                for column in range(len(row)):
-                    subtracted_row.append(row[column])
-            elif row_index == base_row_index:
-                for column in range(len(row)):
-                    base_row.append(row[column])
-        
-        for i in range(len(subtracted_row)): #mnozymy odjemniki przez wielokrotnosc wiersza ktora chcemy odjac
-            subtracted_row[i] = subtracted_row[i] * amount
-        
-        for i in range(len(base_row)): #odejmujemy wiersze od siebie
-            base_row[i] = base_row[i] - subtracted_row[i]
-
-        for row_index, row in enumerate(matrix): #kopiujemy wiersz po odejmowaniu do macierzy
-            if row_index == base_row_index:
-                for column in range(len(row)):
-                    row[column] = base_row[column]
+    def isCalculable(self, row, result):
+        for i in row:
+            if round(i, 8) != 0:
+                return 0 #zwraca 0, jesli uklad wedlug tego wiersza jest oznaczony
+        if round(result, 8) != 0:
+            return 1 #zwraca 1 dla ukladu sprzecznego
+        return 2 #zwraca 2 dla ukladu nieoznaczonego
+    
+    def Jordan(self, matrix, result, dimension):
+        lista_buforowa = [0] * dimension
+        for k in range(dimension): #k oznacza indeks kolumny
+            for row in matrix:  #fragment sprawdzajacy czy na tym etapie mozna uklad uznac za sprzeczny lub nieoznaczony
+                if self.isCalculable(row, result[k]) == 1:
+                    print("Układ sprzeczny")
+                    return None
+                elif self.isCalculable(row, result[k]) == 2:
+                    print("Układ nieoznaczony")
+                    return None
+            max = abs(matrix[k][k]) #ustawiamy wiersze, by tworzyly macierz przekatniowo dominujaca(nie gwarantuje to idealnego rezultatu, ale bedzie zblizony)
+            index = k #szukamy najwiekszego elementu kolumny. Na poczatku zakladamy pierwszy, a potem zmieniamy go, jesli znajdziemy wiekszy
+            for i in range(dimension - k): #i oznacza indeks wiersza. dimension - k jest oznaczeniem ilosci wierszy sprawdzanych
+                if abs(matrix[k + i][k]) > max: # iterujemy od k-tego wiersza do ostatniego. nie bierzemy pod uwage wczesniejszych wierszy, gdyz byloby to kontrproduktywne ze wzcesniejszymi iteracjami
+                    max = abs(matrix[k + i][k])
+                    index = k + i
+            if index != k:
+                #for i in range(dimension):
+                 #   lista_buforowa[i] = matrix[k][i]    # zapisujemy aktualny wiersz podmacierzy w liście buforowej
+                  #  matrix[k][i] = matrix[index][i]   # aktualny wiersz zamieniamy z wierszem z elementem podstawowym
+                   # matrix[index][i] = lista_buforowa[i]
+                self.switch_rows(matrix, k, index) #jesli pierwszy element nie byl najwiekszy dokonujemy zamiany wierszy
+                aux = result[index] #pamietajmy, ze przy zamianie wierszy nalezy tez zamienic elementy w tablicy wynikowej
+                result[index] = result[k]
+                result[k] = aux
+            akk = matrix[k][k] #akk-element podstawowy
+            result[k] /= akk
+            for j in range(dimension - k):
+                matrix[k][k + j] /= akk
+            for i in range(dimension):
+                if i != k:
+                    aik = matrix[i][k]
+                    result[i] -= result[k] * aik
+                    for j in range(dimension):
+                         matrix[i][j] -= matrix[k][j] * aik     
+        result = [round(x, 8) for x in result]
+        return result
+            
--- exercise 2/main.py
+++ exercise 2/main.py
@@ -28,51 +28,24 @@ numbers = extract_numbers_from_file(file_path)
 print("Numbers extracted from the file:", numbers)
 
 matrix = np.array
-"""
-for i in range(dimension):
-    row = []
-    for j in range(2 * dimension + 1):
-        if j < dimension:
-            row.append(numbers[i * (dimension + 1) + j])#tworzy czesc macierzy ze zmiennymi
-        elif j < 2 * dimension:#tworzy czesc macierzy jako macierz jednostkowa
-            if j == dimension + i:
-                row.append(1)
-            else:
-                row.append(0)
-        else:#tworzy ostatnia kolumne macierzy
-            row.append(numbers[i * (dimension + 1) + dimension])
-    matrix.append(row) # matrix przechowuje macierz wszystkich elementow"""
-
-matrix = np.zeros((dimension, 2 * dimension + 1))
+matrix = np.zeros((dimension, dimension))
+result = np.zeros(dimension)
 
 for i in range(dimension):
-    for j in range(2 * dimension + 1):
+    for j in range(dimension + 1):
         if j < dimension:
             matrix[i, j] = numbers[i * (dimension + 1) + j]
-        elif j < 2 * dimension:
-            if j == dimension + i:
-                matrix[i, j] = 1
         else:
-            matrix[i, j] = numbers[i * (dimension + 1) + dimension]
+            result[i] = numbers[i * (dimension + 1) + dimension]
 
-#for row in matrix:
- #   print(row)
-"""
-for column_index, elements in enumerate(zip(*matrix)):
-    for row_index, element in enumerate(elements):
-        if (row_index != column_index and element != 0):
-            
-        elif (row_index == column_index and element != 1):"""
+for row in matrix:
+    print(row)
 
-matrix_tran = matrix.T
+for i in result:
+    print(i)
 
-for column_index, column in matrix_tran:
-    if column_index < dimension:
-        for row_index, element in column:
-            if column_index != row_index and element != 0:
-                for column_index_2, column2 in matrix_tran:
-                    for row_index_2, element in column2:
-                        if column_index == column_index_2 and row_index != row_index_2 and element != 0:
-
-            elif column_index == row_index and element != 1:
+a = dnm()
+b = a.Jordan(matrix, result, dimension)
+if b != None:
+    print("Kolejne wartosci zmiennych to:", b)
                 
