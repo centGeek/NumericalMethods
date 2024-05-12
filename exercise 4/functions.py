e6ae4709c30acce845c3cab5ca206f7979c90acd
WojciechBlyskal <wojtekblyskal03@gmail.com>
2024-05-12 21:58:16 +0200

    wykonane

--- exercise 4/functions.py
+++ exercise 4/functions.py
@@ -21,38 +21,117 @@ class Functions:
                         data.append((float(numbers[0]), float(numbers[1]))) #numbers[0] contains xi, numbers[1] contains Ai
         return data
     
-    def complex_newton_cortes(f, a, b, n):
-        if n % 2 != 0:
-            raise ValueError("Liczba podprzedziałów musi być parzysta")
+    def give_polynomial(self, stopien):
+        wielomian = []
+        for i in range(stopien + 1):
+            wspolczynnik_string = input("Podaj współczynnik przy x^" + str(i) + ": ")
+            wspolczynnik = float(wspolczynnik_string)
+            wielomian.append(wspolczynnik)
+        wielomian.reverse()
+        return wielomian
+    
+    def horner(polynominal):
+        def horner(x):
+            result = 0
+            for i in range(len(polynominal) - 1, -1, -1):
+                result = result * x + polynominal[i]
+            return result
+        return horner
+    
+    """def horner_in_place(x, polynominal_list):
+        result = 0
+        for i in range(len(polynominal_list) - 1, -1, -1):
+            result = result * x + polynominal_list[i]
+        return result
+    
+    def horner_in_place_weight(x, polynominal_list):
+        result = 0
+        for i in range(len(polynominal_list) - 1, -1, -1):
+            result = result * x + polynominal_list[i]
+        return ((math.e ** ((-1) *x ** 2)) * result)"""
+       
+    def complex_newton_cotes(self, f, a, b, accuracy, weight):
+        n = 1
+        condition = True
+        previousSum = float('-inf')
+        while condition:
+            sum = 0.0
+            sumEven = 0.0
+            sumOdd = 0.0
+
+            n = n * 2
+            h = (b - a) / n
+            amount = n + 1
+            fx = []
+
+            for i in range(amount):
+                if weight == 1:
+                    fx.append(f(a + i * h) * self.weight(a + i * h))
+                else:
+                    fx.append(f(a + i * h))
+                if i % 2 == 0 and i != 0 and i != (amount - 1):
+                    sumEven = sumEven + 2 * fx[i]
+                elif i % 2 != 0 and i != 0 and i != (amount - 1):
+                    sumOdd = sumOdd + 4 * fx[i]
+
+            sum = (h / 3) * (fx[0] + fx[amount - 1] + sumEven + sumOdd)
+            diff = math.fabs(sum - previousSum)
+            print(str(sum)+ " " + str(previousSum) + " " + str(diff))
+            previousSum = sum
+            print(str(accuracy) + " " + str(accuracy - diff) + '\n')
+            if math.fabs(diff) < accuracy:
+                condition = False
+
+        return sum
         
-        h = (b - a) / n
-        suma = f(a) + f(b)
+    def limes (self, f, accuracy, weight):
+        lim = 5.0
+        temp = 0.0
+        result = 0.0
 
-        for i in range(1, n, 2):
-            suma += 4 * f(a + i * h)
+        a = 0.0
+        b = lim
+        condition = True
 
-        for i in range(2, n-1, 2):
-            suma += 2 * f(a + i * h)
+        while condition:
+            temp = self.complex_newton_cotes(self, f, a, b, accuracy, weight)
+            result += temp
+            a = b
+            b = b + ((lim - b) / 2)
+            if math.fabs(temp) < accuracy:
+                condition = False
 
-        wynik = (h / 3) * suma
-        return wynik
+        a = (-1) * lim
+        b = 0.0
+        condition = True
+
+        while condition:
+            temp = self.complex_newton_cotes(self, f, a, b, accuracy, weight)
+            result += temp
+            b = a
+            a = a - ((lim - math.fabs(b)) / 2)
+            if math.fabs(temp) < accuracy:
+                condition = False
+
+        return result
 
     def gauss_hermite(data, fun):
         sum = 0
         for i in range(len(data)):
-            #print(str(data[i][0]) + " " + str(data[i][1]))
             sum += data[i][0] * fun(data[i][1])
-            #print(str(sum) + '\n')
         return sum
     
     def error(n):
         return math.factorial(n + 1) * math.sqrt(math.pi) / (2 ** (n + 1)) / math.factorial(2 * n + 2)
 
-    def function(x):
-        return x**2
-    
-    def cosinus(x):
+    def linear(x):
+        return x * 2 - 1
+
+    def trygonometric(x):
         return math.cos(2 * x * x + 1)
     
-    def test(x):
-        return math.sqrt(4 - x * x)
+    def absolute(x):
+        return math.fabs(x + 2) - 3
+    
+    def weight(x):
+        return (math.e ** ((x ** 2) * - 1))
--- exercise 4/main.py
+++ exercise 4/main.py
@@ -2,55 +2,42 @@ import numpy as np
 import os
 from functions import Functions as fun
 
-a = float('-inf')
-b = float('inf')
+#a = float('-inf')
+#b = float('inf')
 value = 0
 
 choice_function = input("Podaj nr funkcji ktora chcesz wybrac: \n'1' oznacza liniową \n'2' oznacza wielomianową \n'3' oznacza trygonometryczną\n'4' oznacza z modulem z argumentu\n")
 if choice_function == '1':
     chosen_function = fun.linear
-    #chosen_function_weigth = fun.linear_weight
+    #chosen_function_weight = fun.linear_weight
 elif choice_function == '2':
     level_string = input("Podaj stopien wielomianu ktory chcesz zbadac:")
     level = int(level_string) 
-    polynomial_list = fun.give_polynomial(level)
-    #chosen_function = fun.horner(polynomial_list)
+    polynomial_list = fun.give_polynomial(fun, level)
+    chosen_function = fun.horner(polynomial_list)
+    #chosen_function_weight = fun.linear_weight(polynomial_list)
 elif choice_function == '3':
     chosen_function = fun.trygonometric
-    #chosen_function_weigth = fun.trygonometric_weight
+    #chosen_function_weight = fun.trygonometric_weight
 else:
     chosen_function = fun.absolute
-    #chosen_function_weigth = fun.absolute_weight
+    #chosen_function_weight = fun.absolute_weight
 
 choice_method = input("Podaj sposob calkowania, ktory chcesz wybrac: \n'1' jesli metode Newtona-Cotesa(Simpsona)\ncokolwiek innego jesli Gaussa Hermita\n")
-if choice_method == 1:
-    chosen_method = 1
+if choice_method == '1':
     choice_weight = input("Czy chcesz zastosowac wage e^(-x^2)? Jesli tak napisz '1', jesli nie napisz cokolwiek innego:\n")
-    accuracy = input("Podaj oczekiwana dokladnosc:\n")
+    accuracy_string = input("Podaj oczekiwana dokladnosc:\n")
+    accuracy = float(accuracy_string)
     if choice_weight == '1':
-        value = fun.complex_newton_cotes(chosen_function, a, b, accuracy, 1)
+        value = fun.limes(fun, chosen_function, accuracy, 1)
     else:
-        value = fun.complex_newton_cotes(chosen_function, a, b, accuracy, 2)
+        value = fun.limes(fun, chosen_function, accuracy, 2)
 else:
-    chosen_method = 2
+    number_string = input("Podaj ile chcesz przedzialow calkowania:\n")
+    number = int(number_string)
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(script_directory, 'hermite.txt')
+    data = fun.read_data_from_file(filename, number + 1) # + 1 as data in file shows n = 2 when it should be n = 1.
+    value = fun.gauss_hermite(data, chosen_function)
 
-
-
-
-accuracy = input("Podaj dokładność do obliczania: ")
-
-chosen_function = fun.test
-
-n = 100
-result = fun.complex_newton_cortes(chosen_function, a, b, n)
-print("Wynik całkowania:", result)
-
-p = 1
-script_directory = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(script_directory, 'hermite.txt')
-
-data = fun.read_data_from_file(filename, p + 1) # + 1 as data in file shows n = 2 when it should be n = 1.
-print(data)
-
-print(fun.gauss_hermite(data, chosen_function))
-print(fun.error(p))
+print("Wynik całkowania:", value)
